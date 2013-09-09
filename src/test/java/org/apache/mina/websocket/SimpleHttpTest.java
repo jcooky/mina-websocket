749f5cc67305ec0acc00c517bfbc816a6315cd9f
jcooky <bak723@gmail.com>
2013-09-09 20:22:54 +0900

    Fix pom.xml Delete SimpleTest and websocket test for temporaly

--- src/test/java/org/apache/mina/websocket/SimpleHttpTest.java
+++ src/test/java/org/apache/mina/websocket/SimpleHttpTest.java
@@ -1,5 +1,10 @@
 package org.apache.mina.websocket;
 
+import static org.junit.Assert.assertEquals;
+
+import java.net.InetSocketAddress;
+import java.util.Collections;
+
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
@@ -8,27 +13,18 @@ import org.apache.mina.core.service.IoHandler;
 import org.apache.mina.core.session.IdleStatus;
 import org.apache.mina.core.session.IoSession;
 import org.apache.mina.http.HttpServerCodec;
-import org.apache.mina.http.api.*;
+import org.apache.mina.http.api.DefaultHttpResponse;
+import org.apache.mina.http.api.HttpRequest;
+import org.apache.mina.http.api.HttpResponse;
+import org.apache.mina.http.api.HttpStatus;
+import org.apache.mina.http.api.HttpVersion;
 import org.apache.mina.transport.socket.nio.NioSocketAcceptor;
 import org.junit.Before;
 import org.junit.Test;
-import org.jwebsocket.client.cgi.CGIClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.net.InetSocketAddress;
-import java.util.Collections;
-
-import static org.junit.Assert.assertEquals;
-
-/**
- * Created with IntelliJ IDEA.
- * User: dennis
- * Date: 13. 8. 28
- * Time: 오후 12:29
- * To change this template use File | Settings | File Templates.
- */
-public class SimpleTest {
+public class SimpleHttpTest {
 	private Logger logger = LoggerFactory.getLogger(getClass());
 
 	class handler implements IoHandler {
@@ -91,16 +87,4 @@ public class SimpleTest {
 		assertEquals(404, response.getStatusLine().getStatusCode());
 	}
 
-	@Test
-	public void testWebSocket() throws Exception {
-		NioSocketAcceptor server = new NioSocketAcceptor();
-
-		server.setHandler(new handler());
-		server.setReuseAddress(true);
-		server.getSessionConfig().setReadBufferSize(2048);
-		server.getFilterChain().addLast("http", new HttpServerCodec());
-		server.bind(new InetSocketAddress(11024));
-
-		CGIClient client = new CGIClient();
-	}
 }
